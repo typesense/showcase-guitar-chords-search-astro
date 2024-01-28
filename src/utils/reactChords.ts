4303a9a24c2e20137c61f63304d008e12f708586
Phi Hung Hoang <hphihung795@gmail.com>
2024-01-28 11:06:29 +0700

    set up instantsearch.js and styling

---
+++ src/utils/reactChords.ts
@@ -0,0 +1,25 @@
+import { renderToString } from 'react-dom/server';
+// @ts-ignore
+import Chord from '@tombatossals/react-chords/lib/Chord';
+export type _chordPosition = {
+  frets: number[];
+  fingers: number[];
+  barres: number[];
+  capo?: boolean;
+};
+export default function RenderChord(chord: _chordPosition) {
+  return renderToString(
+    Chord({
+      chord,
+      instrument: {
+        strings: 6,
+        fretsOnChord: 4,
+        name: 'Guitar',
+        keys: [],
+        tunings: {
+          standard: ['E', 'A', 'D', 'G', 'B', 'E'],
+        },
+      },
+    })
+  );
+}
---
+++ src/utils/typesense.ts
@@ -0,0 +1,18 @@
+import TypesenseInstantSearchAdapter from 'typesense-instantsearch-adapter';
+
+export const typesenseInstantsearchAdapter = new TypesenseInstantSearchAdapter({
+  server: {
+    apiKey: import.meta.env.PUBLIC_TYPESENSE_SEARCH_ONLY || 'xyz',
+    nodes: [
+      {
+        host: import.meta.env.PUBLIC_TYPESENSE_HOST || 'localhost',
+        port: parseInt(import.meta.env.PUBLIC_TYPESENSE_PORT || '8108'),
+        protocol: import.meta.env.PUBLIC_TYPESENSE_PROTOCOL || 'http',
+      },
+    ],
+  },
+  additionalSearchParameters: {
+    query_by: 'key,suffix',
+    num_typos: 0,
+  },
+});
