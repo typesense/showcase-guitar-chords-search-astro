8d1d427186693284128c02d53bd5aa13df232c5b
Phi Hung Hoang <hphihung795@gmail.com>
2024-01-31 18:58:07 +0700

    fix: change env variable naming

--- src/utils/typesense.ts
+++ src/utils/typesense.ts
@@ -2,7 +2,7 @@ import TypesenseInstantSearchAdapter from 'typesense-instantsearch-adapter';
 
 export const typesenseInstantsearchAdapter = new TypesenseInstantSearchAdapter({
   server: {
-    apiKey: import.meta.env.PUBLIC_TYPESENSE_SEARCH_ONLY || 'xyz',
+    apiKey: import.meta.env.PUBLIC_TYPESENSE_SEARCH_ONLY_API_KEY || 'xyz',
     nodes: [
       {
         host: import.meta.env.PUBLIC_TYPESENSE_HOST || 'localhost',
